a12b6661a73a11a673161f74ad76f36d2e8a9223
Muflih Kholidin <muflic.24@gmail.com>
2020-03-29 21:46:14 +0700

    Add example and update README.md

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String []arg) {
-        String result = TextSpace.addSpace(10, "Testasaoeuh", "-", TextSpace.Align.CENTER);
-        System.out.println(result);
+        System.out.println("- Left");
+        System.out.println(TextSpace.addSpace(20, "Muhammad", "-", TextSpace.Align.LEFT));
+        System.out.println(TextSpace.addSpace(20, "Muflih", "-", TextSpace.Align.LEFT));
+        System.out.println(TextSpace.addSpace(20, "Kholidin", "-", TextSpace.Align.LEFT));
+        System.out.println();
+        System.out.println("- Center");
+        System.out.println(TextSpace.addSpace(20, "Muhammad", "-", TextSpace.Align.CENTER));
+        System.out.println(TextSpace.addSpace(20, "Muflih", "-", TextSpace.Align.CENTER));
+        System.out.println(TextSpace.addSpace(20, "Kholidin", "-", TextSpace.Align.CENTER));
+        System.out.println();
+        System.out.println("- Right");
+        System.out.println(TextSpace.addSpace(20, "Muhammad", "-", TextSpace.Align.RIGHT));
+        System.out.println(TextSpace.addSpace(20, "Muflih", "-", TextSpace.Align.RIGHT));
+        System.out.println(TextSpace.addSpace(20, "Kholidin", "-", TextSpace.Align.RIGHT));
+        System.out.println();
+        System.out.println("- Mix");
+        System.out.println(
+            TextSpace.addSpace(10, "Sate Ayam", " ", TextSpace.Align.LEFT)
+            + TextSpace.addSpace(10, "Rp.20000", " ", TextSpace.Align.RIGHT)
+        );
     }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+## Text Lib
+    Add Space
+
+ Example:
+ - Left
+
+        Muhammad------------
+        Muflih--------------
+        Kholidin------------
+
+- Center
+
+        ------Muhammad------
+        -------Muflih-------
+        ------Kholidin------
+
+- Right
+
+        ------------Muhammad
+        --------------Muflih
+        ------------Kholidin
+
+- Mix with " " Separator
+
+        Sate Ayam   Rp.20000
